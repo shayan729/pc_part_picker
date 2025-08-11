7b40926c590ef21d98fabd20ae49e45d9a00566f
[shayan ahmad] <ahmadshayan200429@gmail.com>
2025-08-11 17:36:19 +0530

    pc part picker upload

---
+++ src/project2/MyTableModel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author Akbar Chaudhary
+ */
+class MyTableModel {
+
+    public MyTableModel(ResultSet rs) {
+    }
+    
+}
---
+++ src/project2/ScrollBarCustom.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package project2;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JScrollBar;
+
+/**
+ *
+ * @author Akbar Chaudhary
+ */
+class ScrollBarCustom extends JScrollBar {
+
+    public ScrollBarCustom() {
+        setUI(new ModernScrollBarUI());
+        setPreferredSize(new Dimension(8, 8));
+        setForeground(new Color(48, 144, 216));
+        setBackground(new Color(30,30,30));
+    }
+    
+}
